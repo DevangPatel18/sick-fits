886c14a871597444da4da8324eb77712f1fcfc16
dev637 <devang713@gmail.com>
2018-10-18 15:23:10 -0400

    Module 26 & 27 completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken')
 
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
@@ -6,9 +7,18 @@ const db = require('./db');
 
 const server = createServer();
 
-// TODO User express middleware to handle cookies (JWT)
 server.express.use(cookieParser());
-// TODO User express middleware to populate current user
+
+// decode the JWT so we can get the user Id on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if(token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId onto the req for future request to access
+    req.userId = userId;
+  }
+  next()
+})
 
 server.start(
   {
--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,5 +1,5 @@
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
@@ -8,8 +8,8 @@ const Mutations = {
     const item = await ctx.db.mutation.createItem(
       {
         data: {
-          ...args,
-        },
+          ...args
+        }
       },
       info
     );
@@ -24,17 +24,20 @@ const Mutations = {
     // remove the ID from the updates
     delete updates.id;
     // run the update method
-    return ctx.db.mutation.updateItem({
-      data: updates,
-      where: {
-        id: args.id,
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
       },
-    }, info);
+      info
+    );
   },
   async deleteItem(parent, args, ctx, info) {
     const where = { id: args.id };
     // 1. find the item
-    const item = await ctx.db.query.item({ where }, `{ id title }`)
+    const item = await ctx.db.query.item({ where }, `{ id title }`);
     // 2. Check if they own that item, or have the permissions
     // TODO
     // 3. Delete it!
@@ -46,21 +49,45 @@ const Mutations = {
     // hash their password
     const password = await bcrypt.hash(args.password, 10);
     // create the user in the database
-    const user = await ctx.db.mutation.createUser({
-      data: {
-        ...args,
-        password,
-        permissions: { set: ['USER'] }
-      }
-    }, info)
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ["USER"] }
+        }
+      },
+      info
+    );
     // create the JWT token for them
-    const token = jwt.sign({ userId: user.id}, process.env.APP_SECRET);
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
     // We set the jwt as a cookie on the response
-    ctx.response.cookie('token', token, {
+    ctx.response.cookie("token", token, {
       httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
-    })
+      maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+    });
     // Finally we return the user to the browser
+    return user;
+  },
+  async signin(parent, { email, password }, ctx, info) {
+    // 1. check if there is a user with that email
+    const user = await ctx.db.query.user({ where: { email } });
+    if (!user) {
+      throw new Error("No such user found for email ${email}");
+    }
+    // 2. check if their password is correct
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error("Invalid Password!");
+    }
+    // 3. generate the JWT Token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // 4. Set the cookie with the token
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    });
+    // 5. Return the user
     return user;
   }
 };
